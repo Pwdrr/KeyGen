f43c159e43ca4b5612d54dee27c4ed53df2980e1
Pwdrr <pdumrongrat@gmail.com>
2025-04-04 14:54:01 +0700

    Update KeyGen.py

--- KeyGen.py
+++ KeyGen.py
@@ -29,7 +29,7 @@ def generate_keywords_from_image(image_file):
     image = Image.open(image_file)
 
     # ✅ ใช้โมเดล gemini-pro-vision ที่รองรับภาพ
-    model = genai.GenerativeModel("gemini-2.5-pro")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(
         [image, "Can you suggest some keywords to search for similar images for design reference?"]
     )
